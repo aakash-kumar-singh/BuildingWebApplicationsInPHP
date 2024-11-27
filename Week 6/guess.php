a172944e814cea3f789fc26dc88da5b7e779ae00
Aakash Kumar Singh <118649988+aakash-kumar-singh@users.noreply.github.com>
2024-11-27 10:28:54 +0530

    Update guess.php

--- Week 6/guess.php
+++ Week 6/guess.php
@@ -1,10 +1,10 @@
 <html>
 <body>
-<title> Anamitra Musib 41884c79 </title>
+<title> Aakash Kumar Singh ac884e1c </title>
 <h1> Welcome to my guessing game</h1>
 
 <?php
-$correctnumber=26; //change this number //
+$correctnumber=19; //change this number //
 if(isset($_GET['guess']))
 {
   if (is_numeric($_GET['guess'])===FALSE)
